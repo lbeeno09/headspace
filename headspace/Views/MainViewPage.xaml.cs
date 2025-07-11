7bb3fc0c7e11be67a2c40038ac7c787564d26e4f
lbeeno09 <33786611+lbeeno09@users.noreply.github.com>
2025-07-12 01:32:36 +0900

    allow ctrl+(shift)+tab to shift between components

--- headspace/Views/MainViewPage.xaml.cs
+++ headspace/Views/MainViewPage.xaml.cs
@@ -2,9 +2,13 @@ using headspace.Services.Implementations;
 using headspace.Services.Interfaces;
 using headspace.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using System.Linq;
+using Windows.System;
+using Windows.UI.Core;
 
 namespace headspace.Views
 {
@@ -33,11 +37,54 @@ namespace headspace.Views
 
             // Set custom titlebar
             App.MainWindow.SetTitleBar(AppMenuBar);
+
+            this.Focus(FocusState.Programmatic);
         }
 
         private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
             ViewModel.NavigateCommand.Execute(args);
+        }
+
+        private void MainViewPage_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            var ctrlState = InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Control);
+            bool isCtrlDown = ctrlState.HasFlag(CoreVirtualKeyStates.Down);
+
+            if(isCtrlDown && e.Key == VirtualKey.Tab)
+            {
+                e.Handled = true;
+
+                var shiftState = InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Shift);
+                bool isShiftDown = shiftState.HasFlag(CoreVirtualKeyStates.Down);
+
+                int totalItems = SidebarView.MenuItems.Count; // we have something
+                int currentIndex = SidebarView.MenuItems.IndexOf(SidebarView.SelectedItem);
+                if(currentIndex < 0)
+                {
+                    currentIndex = 0;
+                }
+
+                int nextIndex;
+                if(isShiftDown)
+                {
+                    nextIndex = (currentIndex - 1 + totalItems) % totalItems;
+                }
+                else
+                {
+                    nextIndex = (currentIndex + 1) % totalItems;
+                }
+
+                if(SidebarView.MenuItems[nextIndex] is NavigationViewItem nextItem)
+                {
+                    SidebarView.SelectedItem = nextItem;
+                    if(nextItem.Tag is string pageTag)
+                    {
+                        var navigationService = ((App)Application.Current).Services.GetRequiredService<INavigationService>();
+                        navigationService.NavigateTo(pageTag);
+                    }
+                }
+            }
         }
     }
 }
